69501fcdf509001ce6c579856ea9d2c1e4540e56
Solder&Code <alexey.morozov.2023@mail.ru>
2026-02-10 22:00:38 +0300

    Update M_RGB.h

--- M_RGB.h
+++ M_RGB.h
@@ -1,11 +1,13 @@
 #pragma once
 #include <Arduino.h>
 
-class LED {
-public:
-
-  LED(uint8_t Rpin, uint8_t Gpin, uint8_t Bpin)
-    : _Rpin(Rpin), _Gpin(Gpin), _Bpin(Bpin) {}
+template<uint8_t Rpin, uint8_t Gpin, uint8_t Bpin>
+struct LED {
+  LED() {
+    pinMode(Rpin, OUTPUT);
+    pinMode(Gpin, OUTPUT);
+    pinMode(Bpin, OUTPUT);
+  }
 
   void setHUE(byte color) {                                    // пространство RGB
     if (color < 85) setRGB(85 - color, color, 0);              // если значение цвета меньше 85 то, установить такие цвета
@@ -14,12 +16,8 @@ public:
   }
 
   void setRGB(uint8_t Rclr, uint8_t Gclr, uint8_t Bclr) {  // обновить цвет
-    analogWrite(_Rpin, Rclr);                              // уровень красново
-    analogWrite(_Gpin, Gclr);                              // уровень зелёного
-    analogWrite(_Bpin, Bclr);                              // уровень синего
+    analogWrite(Rpin, Rclr);                               // уровень красново
+    analogWrite(Gpin, Gclr);                               // уровень зелёного
+    analogWrite(Bpin, Bclr);                               // уровень синего
   }
-
-private:
-  // приватные переменные:
-  uint8_t _Rpin, _Gpin, _Bpin;  // пины светодиода
 };
